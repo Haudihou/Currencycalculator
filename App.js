bafcbfeec93e67274b4c1858b63494e696f8073c
Haudihou <newasoft@hotmail.com>
2023-08-30 10:04:43 +0300

    valuuttalaskuri

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [euros, setEuros] = useState('')
+  const [krona, setKrona] = useState(0)
+
+  const calculate = () => {
+    const result = euros.replace(',','.') * 11.85
+    setKrona(result)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Euros</Text>
+      <TextInput 
+        placeholder='Enter amount of euros' 
+        keyboardType='decimal-pad'
+        value={euros}
+        onChangeText={text => setEuros(text)}
+        style={styles.field}
+                />
+      <Text style={styles.field}>Krona</Text>
+      <Text style={styles.field}>{krona.toFixed(2)}</Text>
+      <Button title='Calculate' onPress={calculate}/>
     </View>
   );
 }
@@ -14,7 +31,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+  field: {
+    marginBottom: 10,
+  }
 });
